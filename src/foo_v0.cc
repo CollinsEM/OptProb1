c209a998e27fd387377561affd1c2af1b3673bf9
Eric M. Collins <eric.m.collins@gmail.com>
2022-06-23 23:04:24 -0500

    Initial implementation v0

---
+++ src/foo_v0.cc
@@ -0,0 +1,78 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <omp.h>
+
+#include <test_inputs.h>
+#include <emc_utils.h>
+
+// Version 0.0.0
+//
+// Initial implementation.
+//
+//  * Parse command line arguments
+//    * -v: Verbose output
+//    * -N n: Data array size
+//
+//  * Compute mean and variance using two different methods to verify
+//    correct implementation.
+int main(int argc, char ** argv) {
+  bool verbose = false;
+  // Parse command line arguments
+  for (int i=1; i<argc; ++i) {
+    if (strcmp(argv[i], "-v") == 0) {
+      verbose = true;
+    }
+    else if (strcmp(argv[i], "-N") == 0 && (i+1)<argc) {
+      N = atoi(argv[++i]);
+    }
+  }
+  if (verbose) {
+    for(int i=0; i<argc; ++i) {
+      printf("%s ", argv[i]);
+    }
+    printf("\n");
+  }
+  // Report input vector size
+  if (verbose) printf("N: %d\n", N);
+  
+  // This is a running average calculator that I use quite a lot when
+  // reading in data from a file or a stream without having to store
+  // every value.
+  OnlineAverage<double> xAvg;
+  double t0 = omp_get_wtime();
+  for (int i=0; i<N; ++i) {
+    xAvg += x_test[i%64] + v_test[i%64] + b_test[i%64];
+  }
+  double t1 = omp_get_wtime();
+  
+  // Sanity check to make sure my library is correct
+  double sum = 0.0;
+  for (int i=0; i<N; ++i) {
+    sum += x_test[i%64] + v_test[i%64] + b_test[i%64];
+  }
+  double avg = sum/N;
+  double delta = 0.0;
+  double tmp = 0.0;
+  for (int i=0; i<N; ++i) {
+    delta = x_test[i%64] + v_test[i%64] + b_test[i%64] - avg;
+    tmp += delta*delta;
+  }
+  double var = tmp/(N-1);
+  double dev = sqrt(var);
+  
+  printf("xAvg.count: %ld\n", xAvg.count());
+  printf("xAvg.mean: %f %f\n", xAvg.mean(), avg);
+  printf("xAvg.min: %f\n", xAvg.min());
+  printf("xAvg.max: %f\n", xAvg.max());
+  printf("xAvg.range: %f\n", xAvg.range());
+  printf("xAvg.variance: %f %f\n", xAvg.variance(), var);
+  printf("xAvg.stdDev: %f %f\n", xAvg.stdDev(), dev);
+
+  printf("t1-t0 = %g\n", t1-t0);
+  
+  printf("\n");
+  
+  return 0;
+}
---
+++ src/include/emc_utils.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#include <cmath>
+
+//--------------------------------------------------------------------
+// Kahan's summation algorithm
+//--------------------------------------------------------------------
+template<typename T>
+struct KahanAccumulator {
+  T sum, res;
+  KahanAccumulator() : sum(0), res(0) {
+  }
+  KahanAccumulator(const T & init) : sum(init), res(0) {
+  }
+  template<typename R>
+  KahanAccumulator & operator=(const R & rhs) {
+    sum = rhs;
+    res = 0;
+    return *this;
+  }
+  template<typename R>
+  KahanAccumulator<T> & operator+=(const R & rhs) {
+    T y = rhs - res;
+    T t = sum + y;
+    res = (t - sum) - y;
+    sum = t;
+    return *this;
+  }
+  template<typename R>
+  KahanAccumulator<T> & operator-=(const R & rhs) {
+    T y = -rhs - res;
+    T t = sum + y;
+    res = (t - sum) - y;
+    sum = t;
+    return *this;
+  }
+  template<typename R>
+  KahanAccumulator<T> & operator*=(const R & rhs) {
+    sum *= rhs;
+    res *= rhs;
+    return *this;
+  }
+  template<typename R>
+  KahanAccumulator<T> & operator/=(const R & rhs) {
+    sum /= rhs;
+    res /= rhs;
+    return *this;
+  }
+};
+
+/**
+ * \brief Determine the ongoing average and standard deviation as new
+ * values are accumulated.
+ *
+ * This is an online running average calculator.  As values are
+ * streamed in, they are incorporated into the running mean value (M).
+ * The average is updated using the following recurrance relation:
+ *
+ * a[k] = a[k-1] + (x[k] - a[k-1])/k
+ *
+ * Notice that as k gets very large, the update term (x[k] - a[k-1])/k
+ * gets very small.  If a[k-1] >> (x[k] - a[k-1])/k, then there is a
+ * potential for significant losses in precision due to round-off
+ * error.  To counter this, we incorporate a Kahan summation method to
+ * recover the dropped digits and save them to a correction term (C)
+ * that is incorporated into the next update.
+ */
+template<typename value_t>
+struct OnlineAverage {
+  typedef unsigned long ulong;
+  typedef value_t sum_t;
+
+  ulong N;
+  sum_t M;
+  sum_t S;
+  value_t minVal;
+  value_t maxVal;
+
+public:
+  /**
+   * \brief Default constructor
+   *
+   * Initializes the state for the online average. However, the state
+   * is invalid until the first data point is added.
+   */
+  OnlineAverage() : N(0), M(0), S(0), minVal(1.1e300), maxVal(-1.1e300) {
+  }
+  /**
+   * \brief Operator overload for aggregating values to the set.
+   *
+   * \param[in] x New data value.
+   */
+  template<typename T>
+  OnlineAverage & operator+=(const T x) {
+    N++;
+    sum_t delta(x);
+    delta -= M;
+    M += delta/N;
+    S += delta*(x-M);
+    minVal = ( x < minVal ? x : minVal);
+    maxVal = ( x > maxVal ? x : maxVal);
+    return *this;
+  }
+  /**
+   * \brief Reset the internal state.
+   *
+   * This can be used to restart the averaging process from scratch.
+   */
+  void reset() {
+    N = 0;
+    M = 0;
+    S = 0;
+    minVal =  1.1e300;
+    maxVal = -1.1e300;
+  }
+  /**
+   * \brief Number of aggregated data points.
+   *
+   * \return Number of values that have been aggregated.
+   */
+  ulong count() const {
+    return N;
+  }
+  /**
+   * \brief Arithmetic mean
+   *
+   * \return Arithmetic mean of the aggregated values.
+   */
+  sum_t mean() const {
+    return M;
+  }
+  /**
+   * \brief Minimum value
+   *
+   * \return Minimum of the aggregated values.
+   */
+  value_t min() const {
+    return minVal;
+  }
+  /**
+   * \brief Maximum value
+   *
+   * \return Maximum of the aggregated values.
+   */
+  value_t max() const {
+    return maxVal;
+  }
+  /**
+   * \brief Range of values (max-min)
+   *
+   * \return Difference between the maximum and minimum values.
+   */
+  value_t range() const {
+    return (maxVal - minVal);
+  }
+  /**
+   * \brief Variance
+   *
+   * \return Variance of the aggregated values.
+   */
+  sum_t variance() const {
+    return (N>1 ? S/(N-1) : 0.0);
+  }
+  /**
+   * \brief Standard deviation
+   *
+   * \return Standard deviation of the aggregated values.
+   */
+  sum_t stdDev() const {
+    return std::sqrt(variance());
+  }
+};
